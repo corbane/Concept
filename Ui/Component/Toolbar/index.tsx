423b011febbc450fcd366b81b13d307b29fe35e3
corbane <kitjmv@gmail.com>
2020-04-27 22:35:28 +0200

    cleaning

--- Ui/Component/Toolbar/index.tsx
+++ Ui/Component/Toolbar/index.tsx
@@ -1,33 +1,91 @@
 
-import { ListView } from "../List/index.js"
+import { xnode } from "../../Base/xnode.js"
+import { Container } from "../../Base/Container/index.js"
+import { ExpendableElement, expandable } from "../../Base/expendable.js"
+import { cssFloat } from "../../Base/dom.js"
+import { Component } from "../../Base/Component/index.js"
+
 import { define } from "../../db.js"
 
-type Direction = "lr" | "rl" | "tb" | "bt"
-
-type Units = "px" | "%"
+interface $ListView extends $Container
+{
+     type: "list-view"
+}
 
 declare global
 {
-     export interface $Toolbar extends $Extends <$ListView> // $Container
+     interface $Toolbar extends $Extends <$ListView> // $Container
      {
           type     : "toolbar"
           title    : string
           buttons  : $Button []
      }
+
 }
 
-const toFlexDirection = {
-     lr: "row"            as "row",
-     rl: "row-reverse"    as "row-reverse",
-     tb: "column"         as "column",
-     bt: "column-reverse" as "column-reverse",
-}
+class ListView <$ extends $Extends <$ListView>> extends Container <$>
+{
+     swipeable: ExpendableElement
 
-const toReverse = {
-     lr: "rl" as "rl",
-     rl: "lr" as "lr",
-     tb: "bt" as "bt",
-     bt: "tb" as "tb",
+     /** @override */
+     getHtml ()
+     {
+          if ( this.container != undefined )
+               return [this.container]
+
+          const slot = this.slot = <div class="list-view-slide"></div>
+
+          super.getHtml ()
+
+          const container = this.container
+
+          container.append ( slot )
+          container.classList.add ( "list-view" )
+
+          this.swipeable = expandable ( slot, {
+               handles   : [ container ],
+               minSize  : 0,
+               maxSize  : 0,
+               property  : this.is_vertical ? "top": "left",
+               direction : this.data.direction,
+               unit     : "px",
+               //mouseWheel: true,
+          })
+          this.swipeable.activate ()
+
+          window.addEventListener ( "DOMContentLoaded", () =>
+          {
+               this.swipeable.updateConfig ({
+                    minSize: -this.slideSize (),
+               })
+          })
+
+          return [this.container]
+     }
+
+     onChildrenAdded ( elements: Component [] )
+     {
+          this.swipeable.updateConfig ({
+               minSize  : -this.slideSize (),
+               property : this.is_vertical ? "top": "left",
+               direction: this.data.direction,
+          })
+     }
+
+     private slideSize ()
+     {
+          const { slot } = this
+
+          return cssFloat ( slot, this.is_vertical ? "height" : "width" )
+     }
+
+     swipe ( offset: string|number, unit?: "px" | "%" )
+     {
+         // if ( typeof offset == "string" )
+         //      this.swipeable.swipe ( offset )
+         // else
+         //      this.swipeable.swipe ( offset, unit )
+     }
 }
 
 /**
@@ -71,3 +129,22 @@ export class Toolbar extends ListView <$Toolbar>
 }
 
 define ( Toolbar, ["toolbar"] )
+
+
+// type Direction = "lr" | "rl" | "tb" | "bt"
+//
+// type Units = "px" | "%"
+//
+// const toFlexDirection = {
+//      lr: "row"            as "row",
+//      rl: "row-reverse"    as "row-reverse",
+//      tb: "column"         as "column",
+//      bt: "column-reverse" as "column-reverse",
+// }
+//
+// const toReverse = {
+//      lr: "rl" as "rl",
+//      rl: "lr" as "lr",
+//      tb: "bt" as "bt",
+//      bt: "tb" as "tb",
+// }
